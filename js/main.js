ef815d392eeb183f60e68b97b105e457508195d7
evvvrado <everaldocrj@gmail.com>
2021-05-18 11:54:49 -0300

    Alterações no mobile e formulário

--- js/main.js
+++ js/main.js
@@ -76,3 +76,55 @@ debounce = function(func, wait, immediate) {
 		animeScroll();
 	}, 200));
 })();
+
+
+//Slide
+const iflSlider={
+	redim:false,
+	init:function(slider){
+		iflSlider.ajustaTamanho(slider);
+		slider.find('.sliderBtn button').each(function(idx,btn){
+			$(btn).click(function(event){
+				let pre=$(slider).find('.sliderPreCorpo');
+				let i=$(slider).find('.sliderItem');
+				let w=i.outerWidth(true);
+				let novo=0;
+				if(idx==1 || $(btn).hasClass('proximo'))novo=pre[0].scrollLeft+w;
+				else novo=pre[0].scrollLeft-w;
+				if('scrollBehavior' in document.documentElement.style)pre[0].scroll({left:novo,behavior:'smooth'});
+				else pre.stop(true,true).animate( { scrollLeft : novo } , 300);
+			});
+		});
+	},
+	initTodos:function(){
+		$('.slider').each(function(idxS,slider){
+			iflSlider.init($(slider));
+		});
+	},
+	resizeTodos:function(){
+		$('.slider .sliderItem').css('width','');
+		$('.slider .sliderPreCorpo').css('width','');
+		if(iflSlider.redim){
+			clearTimeout(iflSlider.redim);
+			iflSlider.redim=false;
+		};
+		iflSlider.redim=setTimeout(function(){
+			$('.slider').each(function(idxS,slider){
+				iflSlider.ajustaTamanho($(slider));
+				iflSlider.redim=false;
+			});
+		},100);
+	},
+	ajustaTamanho:function(slider){
+		let pre=$(slider).find('.sliderPreCorpo');
+		if(pre.length){
+			let corpo=$(slider).find('.sliderCorpo');
+			let i=$(slider).find('.sliderItem');
+			let w=i.outerWidth(true);
+			let sw=pre.outerWidth(true);
+			if(sw<w)i.css('width',sw+'px');
+			if(pre[0].offsetWidth<corpo[0].offsetWidth)slider.find('.sliderBtn').css('opacity','');
+			else slider.find('.sliderBtn').css('opacity','0');
+		}
+	}
+}
